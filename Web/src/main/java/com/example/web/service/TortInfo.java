16e2bf2b2234a00dcf6fcbf9ee05485e1e76779f
Saints888 <mikhail.istomin.96@yandex.ru>
2023-08-28 15:19:16 +0300

    tortinfo

--- Web/src/main/java/com/example/web/service/TortInfo.java
+++ Web/src/main/java/com/example/web/service/TortInfo.java
@@ -1,41 +1,58 @@
 package com.example.web.service;
 
+import java.util.Objects;
+
 public class TortInfo {
+    // Класс описывает информацию о торте
+// Включает в себя поля: id, name, description, price, weight
     private String id, name, description;    //идентификатор, название, описание
     private int price;                       //цена
     private int weight;                      //вес
 
-    public TortInfo() {
+    public TortInfo() { // Объявление пустого конструктора TortInfo
     }
 
     public String getId() {
         return id;
     }
+    // Метод getId()
+    // Возвращает значение поля id
+
 
     public void setId(String id) {
         this.id = id;
     }
+    // Метод setId()
+    // Устанавливает значение поля id
 
     public String getName() {
         return name;
     }
+    // Метод getName()
+    // Возвращает значение поля name
 
     public void setName(String name) {
         this.name = name;
     }
+    // Метод setName()
+    // Устанавливает значение поля name
 
     public String getDescription() {
         return description;
     }
+    // Метод getDescription()
+    // Возвращает значение поля description
 
     public void setDescription(String description) {
         this.description = description;
     }
+    // Метод setDescription()
+    // Устанавливает значение поля description
     public int getPrice() {
         return price;
-    }description) {
-        this.description = description;
     }
+    // Метод getPrice()
+    // Возвращает значение поля price
 
 
     public void setPrice(int price) {
@@ -49,6 +66,38 @@ public class TortInfo {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    /**
+     * Создание id города на основе названия и хэшкода торта
+     */
+    public void createId() {
+    this.id = "" + hashCode();
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (this == ob) return true;
+        if (ob == null) return false;
+        if (this.getClass() != ob.getClass())   return false;
+        TortInfo info =(TortInfo) ob;
+        return id.equals(info.id) && name.equalsIgnoreCase(info.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name.toLowerCase());
+    }
+
+    @Override
+    public String toString () {
+        return "TortInfo{" +
+                "id='" + id + '\'' +
+                ", name'" + name +'\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                ", weight" + weight + '}';
+
     }
 }
 
