de7e2802f9eeaa1ec4250755e424ae2450473d97
Saints888 <mikhail.istomin.96@yandex.ru>
2023-08-28 14:07:55 +0300

    errors

---
+++ Web/src/main/java/com/example/web/err/Errors.java
@@ -0,0 +1,25 @@
+package com.example.web.err;
+
+public class Errors {
+// Определение класса Errors
+
+    public static final String DATABASE_ERROR = "DATABASE_ERROR";
+    // Определение константы DATABASE_ERROR для ошибки базы данных
+
+
+    public static final String TORT_NOT_FOUND = "TORT_NOT_FOUND";
+    // Определение константы TORT_NOT_FOUND для ошибки отсутствия информации о торте
+
+    public static final String TORT_ALREADY_EXISTS  = "TORT_ALREADY_EXISTS";
+    // Определение константы TORT_ALREADY_EXISTS для ошибки уже существующего торта
+
+    public static final String ARGUMENT_ERROR = "ARGUMENT_ERROR";   // Определение константы ARGUMENT_ERROR для ошибки некорректного аргумента
+
+
+    public static final String TORT_IMAGE_URL_ERROR = "TORT_IMAGE_URL_ERROR";
+    // Определение константы TORT_IMAGE_URL_ERROR для ошибки связанной с URL изображения торта
+
+
+    public static final String TORT_IMAGE_ERROR = "TORT_IMAGE_ERROR";
+    // Определение константы TORT_IMAGE_ERROR для ошибки связанной с изображением торта
+}
